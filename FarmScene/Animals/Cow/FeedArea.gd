d7fbda2709500b95c69f12b7b9e24cea63732e54
David <44338544+goessm@users.noreply.github.com>
2023-11-04 14:20:40 +0100

    feed the cow

--- FarmScene/Animals/Cow/FeedArea.gd
+++ FarmScene/Animals/Cow/FeedArea.gd
@@ -4,4 +4,8 @@ func _on_area_entered(area: Area2D):
 	if area.is_in_group(Groups.Food):
 		var food = area.get_parent()
 		if food is FoodItem:
-			pass
+			eat_food(food)
+
+func eat_food(food: FoodItem):
+	print("I ate a " + str(food.type))
+	food.get_eaten()
