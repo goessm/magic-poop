6f49d73138688f039c1fd8673d4d6e5a352ffee9
goessm <44338544+goessm@users.noreply.github.com>
2024-08-06 13:46:22 +0200

    Scale up to glorious 1080p

--- Scenes/Remaster/MainMenu/main_menu.gd
+++ Scenes/Remaster/MainMenu/main_menu.gd
@@ -7,4 +7,6 @@ func _on_startgame_button_pressed():
 	get_tree().change_scene_to_packed(game_scene)
 
 func _on_startjam_button_pressed():
+	const jam_window_size = Vector2i(384, 216)
+	get_window().content_scale_size = jam_window_size
 	get_tree().change_scene_to_packed(jam_game_scene)
