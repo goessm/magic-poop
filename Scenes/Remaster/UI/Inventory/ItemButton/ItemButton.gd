d343e960a3f7515c0169d50cf4748c28e92d5744
goessm <44338544+goessm@users.noreply.github.com>
2024-08-06 17:06:10 +0200

    New inventory

---
+++ Scenes/Remaster/UI/Inventory/ItemButton/ItemButton.gd
@@ -0,0 +1,7 @@
+extends Button
+
+@export var item_type: Item.Items
+
+@export_group("Children")
+@export var image: TextureRect
+@export var label: Label
---
+++ Scenes/Remaster/UI/Inventory/Resources/inventory_item.gd
@@ -0,0 +1,13 @@
+class_name Item
+
+extends Resource
+
+enum Items {
+	Poop,
+	PoopRainbow,
+	FoodApple,
+	FoodBanana,
+}
+
+@export var type: Items
+@export var texture: Texture2D
